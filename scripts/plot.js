18eded67eb31fa8a44aca58196bfbe498e007086
Per Hartveit <per_l_hartveit@outlook.com>
2023-07-02 10:12:23 -0700

    encapsulated scatter plotting functionality into a single class

--- scripts/plot.js
+++ scripts/plot.js
@@ -1,26 +1,76 @@
-const getScoresPerPersonAsCoordinates = (data, currentTrack, names) => {
-  return names.map((name) => {
-    const scores = data[name][currentTrack]["scores"];
-    return {
-      "name": name,
-      "coordinates": {
-        "x": Object.keys(scores).map((el) => parseInt(el)),
-        "y": Object.values(scores),
-      }
-    }
-  });
+
+
+const plotter = new TrackScatterPlotter();
+plotter.plot();
+
+
+
+
+class TrackScatterPlotter{
+
+constructor(url = null, layoutConfig = null, htmlElementId= null, currentTrack = null){
+    this.url = url != null ? "https://someuser0111.github.io/data/secondJuli.json" : url;
+    this.layoutConfig = layoutConfig != null ? {
+        title: currentTrack,
+        margin: { t: 0 },
+        xaxis:{
+            tickmode: "linear",
+            // start of xaxis
+            tick0 : 0,
+            // axis step
+            dtick: 1
+        },
+        yaxis:{
+            tickmode: "linear",
+            // start of xaxis
+            tick0 : 0,
+            // axis step
+            dtick: 1
+        }
+      }: layoutConfig
+    this.htmlElementId = htmlElementId != null ? 'tester' : htmlElementId
+    
+    this.currentTrack = currentTrack != null ?  "Lynghaug" : currentTrack;
+    this.date = new Date();
 }
 
-const currentTrack = "Lynghaug";
-const date = new Date();
-const ToCoordinates = async () => {
-  const response = await fetch("https://someuser0111.github.io/data/secondJuli.json");
-  const data = await response.json();  
-  return getScoresPerPersonAsCoordinates(data, currentTrack, Object.keys(data));
+plot = async ()=>{
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const coords = await this.fetchDataAndTransformToCoordinates();
+  const plotlyData = this.toPlotlyDataFormat(coords);
+ 
+  const scatter = document.getElementById(htmlElementId);
+  Plotly.newPlot(scatter, plotlyData, this.layoutConfig);
+    });
 }
+
+fetchDataAndTransformToCoordinates = async (url = null) => {
+    try{
+        const response = url != null ? await fetch(this.url) : await fetch(url);
+        const data = await response.json();  
+        return getScoresPerPersonAsCoordinates(data, currentTrack, Object.keys(data));
+    }
+    catch (error) {
+        console.error(`An error occured when fetching from ${this.url}. Message: ${error}`)
+    }
+  }
+
+  getScoresPerPersonAsCoordinates = (data, currentTrack, names) => {
+    return names.map((name) => {
+      const scores = data[name][currentTrack]["scores"];
+      return {
+        "name": name,
+        "coordinates": {
+          "x": Object.keys(scores).map((el) => parseInt(el)),
+          "y": Object.values(scores),
+        }
+      }
+    });
+  }
 
 // personCoordinates object format: Array<{name:string,coordinates:{x:arr<int>,y:arr<int>}}>
-const toPlotlyDataFormat = (personCoordinates)=>{
+toPlotlyDataFormat = (personCoordinates)=>{
     return personCoordinates.map(personCoords =>{
         return {
             x: personCoords["coordinates"]["x"],
@@ -32,71 +82,4 @@ const toPlotlyDataFormat = (personCoordinates)=>{
         }
     })
 }
-
-document.addEventListener("DOMContentLoaded", async () => {
-  const coords = await ToCoordinates();
-  
-const plotlyData = toPlotlyDataFormat(coords);
- const layoutConfig = {
-    title: currentTrack,
-    margin: { t: 0 },
-    xaxis:{
-        tickmode: "linear",
-        // start of xaxis
-        tick0 : 0,
-        // axis step
-        dtick: 1
-    },
-    yaxis:{
-        tickmode: "linear",
-        // start of xaxis
-        tick0 : 0,
-        // axis step
-        dtick: 1
-    }
-  }
-  const scatter = document.getElementById('tester');
-  Plotly.newPlot(scatter, plotlyData, layoutConfig);
-});
-
-
-
-
-
-
-
-
-
-//     var trace1 = {
-//     x: [1, 2, 3, 4, 5],
-//     y: [1, 6, 3, 6, 1],
-//     mode: 'markers',
-//     type: 'scatter',
-//     name: 'Team A',
-     
-//   };
-  
-//   var trace2 = {
-//     x: [1.5, 2.5, 3.5, 4.5, 5.5],
-//     y: [4, 1, 7, 1, 4],
-//     mode: 'markers',
-//     type: 'scatter',
-//     name: 'Team B',
-//     text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
-//     marker: { size: 12 }
-//   };
-  
-//   var data = [ trace1, trace2 ];
-  
-//   var layout = {
-//     xaxis: {
-//       range: [ 0.75, 5.25 ]
-//     },
-//     yaxis: {
-//       range: [0, 8]
-//     },
-//     title:'Data Labels Hover'
-//   };
-  
-//   Plotly.newPlot('myDiv', data, layout);
-  
+}
