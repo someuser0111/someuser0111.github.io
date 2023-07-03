2ef1c2f5055083996e2e57fa59553a0aef2fc14b
Per Hartveit <per_l_hartveit@outlook.com>
2023-07-02 17:09:40 -0700

    navigation on query param to correct plot

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,79 +1,128 @@
-//////////////////////////////////////////////////////////////////////
-// ALL QUERY PARAM HANDLING FOR GOING TO THE PLOTS
-//////////////////////////////////////////////////////////////////////
-document.querySelector("#"+"linkToPlot").href = "plot.html?date=02072023";
+const isFirefox = () => navigator.userAgent.toLowerCase().includes("firefox")
 
-function formatDateAsDdmmyyyy(date) {
-    // thanks chatgtp
-    
-    // Create a Date object from the input date string
-    const inputDate = new Date(date);
-    // Extract day, month, and year from the Date object
-    const day = inputDate.getDate();
-    const month = inputDate.getMonth() + 1; // January is 0, so we need to add 1
-    const year = inputDate.getFullYear();
-    // Format day and month to have leading zeros if necessary
-    const formattedDay = day.toString().padStart(2, '0');
-    const formattedMonth = month.toString().padStart(2, '0');
-    // Concatenate day, month, and year in the desired order
-    const formattedDate = `${formattedDay}${formattedMonth}${year}`;
-    return formattedDate;
-   }
+const notFirefox = () => navigator.userAgent.toLowerCase().includes("chrome") || navigator.userAgent.toLowerCase().includes("edg");
 
-let sentToDefaultDate = true;
+// const isChrome = () => navigator.userAgent.toLowerCase().includes("chrome")
 
-var currentHrefToPlot = "";
-// newKeyValuePair is of shape {key:string,value:string}
-const updateQueryParamsForHref = (newKeyValuePair)=>{
-    currentHrefToPlot = "";
-    const addedFirstQueryParam = false;
-    const queryParams = document.querySelector("#"+"linkToPlot").href.split("?");
-    if (queryParams.length == 0) return null;
-    const params = new URLSearchParams("?"+queryParams);
-    for (const [key, value] of params) {
-      if (!addedFirstQueryParam){
-        if (newKeyValuePair.key == key){
-            currentHrefToPlot += `?${key}=${value}`
+// const isEdge = () => navigator.userAgent.toLowerCase().includes("edg")
+
+let queryParams = {
+    "track":null,
+    "date":null
+}
+
+const addÅsane = ()=>{
+    // console.log("før",queryParams)
+    const åsane = document.querySelector("#åsane");
+    åsane.parentElement.setAttribute("class","Åsane");
+    document.querySelector("#lynghaug").removeAttribute("class");
+    queryParams["track"] = "Åsane";
+    // console.log("etter",queryParams)
+  }
+  const addLynghaug = ()=>{
+    // console.log("før",queryParams)
+    const lynghaug = document.querySelector("#lynghaug");
+    lynghaug.parentElement.setAttribute("class","Lynghaug");
+    document.querySelector("#åsane").removeAttribute("class");
+    queryParams["track"] = "Lynghaug";
+    // console.log("etter",queryParams)
+
+}
+
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+//////////     DATE INPUT LISTENER  /////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+const datepickerId = "datepicker";
+const dateInput = document.querySelector("#"+datepickerId);
+dateInput.addEventListener("change",e=>{
+    sentToDefaultDate = false;
+    // chrome and edge returns input in the same format. Firefox has different format on return. Double check. Not sure if correct.
+    if (isFirefox()){
+        const yyyymmdd = e.target.value.split("-");
+        const yyyy = yyyymmdd[0];
+        const mm = yyyymmdd[1];
+        const dd = yyyymmdd[2];
+        dateInput.setAttribute("class",dd+mm+yyyy);
+    }
+    else{
+        const yyyymmdd = e.target.value.split("-");
+        const yyyy = yyyymmdd[0];
+        const mm = yyyymmdd[1];
+        const dd = yyyymmdd[2]
+        dateInput.setAttribute("class",dd+mm+yyyy);
+    }
+  })
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+//////////    END DATE INPUT LISTENER  //////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+//////////     GO TO LINK ///////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+document.querySelector("#goToLink").addEventListener("click",e=>{
+    const params = makeQueryParams();
+    if (params == null) {
+        alert("Pick a date and track");
+        return;
+    }
+    let plotUrl = getPlotUrlWithQueryParams(params);
+    console.log(plotUrl);
+    window.location.href = plotUrl;
+});
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+//////////     END GO TO LINK ///////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+
+const makeQueryParams = ()=>{
+    const date = document.querySelector("#datepicker");
+    let dateValue = date.getAttribute("class");
+    if (dateValue == null) return 
+    const track = document.querySelector("#trackselecter").getAttribute("class");
+    if (date == null || track == null) {
+        return null;
+    }
+    return {
+        "date":dateValue,
+        "track":track
+    }
+}
+
+
+const check = ()=>{
+    const date = document.querySelector("#datepicker");
+    console.log(date);
+    console.log(date.getAttribute("class"));
+
+    const track = document.querySelector("#trackselecter");
+    console.log(track);
+    console.log(track.getAttribute("class"));
+}
+
+//queryParams object shaep: {date:ddmmyyyy,track:string}
+const getPlotUrlWithQueryParams =(queryParams, originSite ="index.html")=>{
+    let plotUrl = window.location.href.split(originSite)[0];
+    plotUrl += "plot.html";
+    let firstParam = true;
+    for (const [key,value] of Object.entries(queryParams)){
+        if (firstParam){
+            plotUrl += `?${key}=${value}`;
         }
         else{
-            currentHrefToPlot += `&?${key}=${value}`
+            plotUrl += `&?${key}=${value}`;
         }
-      }
     }
-    alert(currentHrefToPlot);
+    return plotUrl;
   }
-  
-  // setting default href
-  
-  const trackSelecter = document.querySelector("#"+"trackSelecter");
-  trackSelecter.addEventListener("change",e=>{
-    sentToDefaultDate = false;
-    updateQueryParamsForHref({"key":"track","value":e.target.value})
-    document.querySelector("#"+"linkToPlot").href = currentHrefToPlot;
-  });
-  
-  const datepickerId = "datepicker";
-  const dateInput = document.querySelector("#"+datepickerId);
-  dateInput.addEventListener("change",e=>{
-    sentToDefaultDate = false;
-    const date =formatDateAsDdmmyyyy(e.target.value);
-    updateQueryParamsForHref({"key":"date","value":date})
-    document.querySelector("#"+"linkToPlot").href = currentHrefToPlot;
-  })
-  
-  
-  
-  document.querySelector("#"+"linkToPlot").addEventListener("click",e=>{
-    console.log(currentHrefToPlot);
-    if (sentToDefaultDate){alert("Du valgte ingen konkret dato og sendes til første spilte: 2 juli 2023");}
-    else{
-    //   const dateInputValue = document.querySelector("#"+datepickerId).value;
-    //   const trackSelecterValue = document.querySelector("#"+"trackSelecter").value;
-    //   document.querySelector("#"+"linkToPlot").href = `plot.html?date=${dateInputValue}&track=${trackSelecterValue}`;
-    }
-  });
-  
-  //////////////////////////////////////////////////////////////////////
-  // END QUERY PARAM HANDLING FOR GOING TO THE PLOTS
-  //////////////////////////////////////////////////////////////////////
-  
--- scripts/plot.js
+++ scripts/plot.js
@@ -1,8 +1,39 @@
-const url ="https://someuser0111.github.io/data/history/02072023.json";
+
+const getQueryParams = ()=>{
+  const path = window.location.href.split("?");
+  if (path.length == 0) return null;
+  const params = new URLSearchParams("?"+path[1]);
+  for (const [key, value] of params) {
+    if (key == "date"){
+      return value;
+    }
+  }
+  return null;
+}
+
+
+
+
+
+
+let date = getQueryParams();//getDateQueryParam();
+if (date == null){
+    const scatterContainer = document.getElementById("tester");
+    const errorElement = document.createElement("div");
+    const errorText = document.createTextNode("You haven't played that day.");
+    errorElement.appendChild(errorText);
+    document.body.appendChild(errorElement);
+}
+else{
+const defaultDate = "02072023";
+date = date == null ? defaultDate : date;
+
+const url = `https://someuser0111.github.io/data/history/${date}.json`;
 const currentTrack = "Lynghaug";
 const layoutConfig ={
     margin: { t: 0 },
     xaxis:{
+      name:"Track number",
         tickmode: "linear",
         // start of xaxis
         tick0 : 0,
@@ -10,6 +41,7 @@ const layoutConfig ={
         dtick: 1
     },
     yaxis:{
+      name: "Shots per track",
         tickmode: "linear",
         // start of xaxis
         tick0 : 0,
@@ -19,15 +51,13 @@ const layoutConfig ={
   };
 
 const fetchDataAndTransformToCoordinates = async (url) => {
-    try{
         const response = await fetch(url);
+        if (!response.ok){
+          throw new Error(`AN ERROR OCCURED FETCHING FROM ${url}.\nStatus code: ${response.status}. Reason: ${response.statusText}`)
+        }
         const data = await response.json();  
         return getScoresPerPersonAsCoordinates(data, currentTrack, Object.keys(data));
     }
-    catch (error) {
-        console.error(`An error occured when fetching from ${this.url}. Message: ${error}`)
-    }
-  }
 const getScoresPerPersonAsCoordinates = (data, currentTrack, names) => {
   return names.map((name) => {
     const scores = data[name][currentTrack]["scores"];
@@ -56,10 +86,19 @@ const toPlotlyDataFormat = (personCoordinates)=>{
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const coords = await fetchDataAndTransformToCoordinates(url);
-  const plotlyData = await toPlotlyDataFormat(coords);
   const scatter = document.getElementById("tester");
-  Plotly.newPlot(scatter, plotlyData, layoutConfig);
-    });
-
-
+  try{
+    const coords = await fetchDataAndTransformToCoordinates(url);
+    const plotlyData = await toPlotlyDataFormat(coords);
+    Plotly.newPlot(scatter, plotlyData, layoutConfig);
+    
+    }
+  catch(error){
+    const errorElement = document.createElement("div");
+    const errorText = document.createTextNode("You haven't played that day.");
+    errorElement.appendChild(errorText);
+    scatter.appendChild(errorElement);
+    }
+  }
+);
+}
